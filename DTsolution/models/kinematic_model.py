c311715f757c44a881e687daeabf3f56f867fb0a
losmobilos3 <losmobilos3@gmail.com>
2026-04-06 17:06:00 +0200

    Updated new kinematic model to also work when different joints have different angle differences.

--- DTsolution/models/kinematic_model.py
+++ DTsolution/models/kinematic_model.py
@@ -19,7 +19,7 @@ class KinematicModel:
         self.moving = False
         self.current_movement_start_time = 0.0
         self.current_movement_duration = 0.0
-        self.current_acceleration_duration = 0.0
+        self.current_acceleration_durations = np.array([0.0] * 6)
 
         # Experiment variables
         self.start_time = 0.0
@@ -28,6 +28,7 @@ class KinematicModel:
     def _determine_movement_duration_and_acceleration_duration(self, start_angles: list, end_angles: list) -> float:
         # Determine angle differences
         delta_x = np.abs(np.array(end_angles) - np.array(start_angles))
+        max_delta_index = np.argmax(delta_x)
         max_delta_x = np.max(delta_x)
 
         # IDEAL CASE: t_acc <= T/3
@@ -41,7 +42,7 @@ class KinematicModel:
 
         # Set IDEAL values
         movement_duration = t_cruise + 2 * t_acc
-        acceleration_duration = t_acc
+        max_acceleration_duration = t_acc
 
         # NON-IDEAL CASE check: t_acc > T/3
         if t_acc > t_cruise:
@@ -51,9 +52,18 @@ class KinematicModel:
 
             # Overwrite IDEAL values with NON-IDEAL values
             movement_duration = 3*tau
-            acceleration_duration = tau
+            max_acceleration_duration = tau
 
-        return movement_duration, acceleration_duration
+        acceleration_durations = np.zeros_like(self.current_joint_velocities)
+        acceleration_durations[max_delta_index] = max_acceleration_duration
+        # acceleration_coefficients = np.zeros_like(self.current_joint_velocities)
+        # acceleration_coefficients[max_delta_index] = self.a_max
+
+        # Calculate the acceleration durations and acceleration coefficients of the rest of the joints to make sure they all finish the movement at the same time
+        max_joint_velocities = (movement_duration + np.sqrt(movement_duration**2 - 4 * delta_x / self.a_max)) / (2/self.a_max)
+        acceleration_durations = max_joint_velocities / self.a_max
+
+        return movement_duration, acceleration_durations
 
 
     def fmi2Instantiate(self):
@@ -70,7 +80,7 @@ class KinematicModel:
     def fmi2SetCommandedJointAngles(self, angles: list):
         # Setup variables for move to be made
         self.commanded_joint_angles = np.array(angles)
-        self.current_movement_duration, self.current_acceleration_duration = self._determine_movement_duration_and_acceleration_duration(self.current_joint_angles, self.commanded_joint_angles)
+        self.current_movement_duration, self.current_acceleration_durations = self._determine_movement_duration_and_acceleration_duration(self.current_joint_angles, self.commanded_joint_angles)
 
     def fmi2StartMovement(self):
         self.moving = True
@@ -89,16 +99,21 @@ class KinematicModel:
                 self.current_joint_velocities = np.array([0.0] * 6)
                 self.moving = False
             else: # Else keep on doing the movement
-                # Accelerating
-                if t_rel <= self.current_acceleration_duration:
-                    self.current_joint_velocities += self.a_max * step_size
-                # Cruising
-                elif t_rel < self.current_movement_duration - self.current_acceleration_duration:
-                    # No change in velocity
-                    pass
-                # Decelerating
-                else:
-                    self.current_joint_velocities -= self.a_max * step_size
+                # Determine which joints are accelerating, cruising and decelerating
+                is_accelerating = t_rel <= self.current_acceleration_durations
+                is_decelerating = t_rel >= (self.current_movement_duration - self.current_acceleration_durations)
+                is_cruising = ~(is_accelerating | is_decelerating)
+
+                # Update volicities based on masks from above
+                # Accelerating: add acceleration
+                self.current_joint_velocities[is_accelerating] += self.a_max * step_size
+
+                # Decelerating: subtract acceleration
+                self.current_joint_velocities[is_decelerating] -= self.a_max * step_size
+
+                # Cruising: (Implicitly does nothing, no code needed)
+
+                # Update positions based on the velocity of each joint
                 self.current_joint_angles += self.current_joint_velocities * step_size
 
     def fmi2GetJointPositions(self) -> list: 
