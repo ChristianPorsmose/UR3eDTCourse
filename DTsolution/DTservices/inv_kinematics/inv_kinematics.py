40d38bd58963128cefab79d1193eb91b642f5ad6
losmobilos3 <losmobilos3@gmail.com>
2026-05-05 10:38:58 +0200

    Implemented inv_kinematics service to actually work

--- DTsolution/DTservices/inv_kinematics/inv_kinematics.py
+++ DTsolution/DTservices/inv_kinematics/inv_kinematics.py
@@ -58,23 +58,48 @@ class BreakableRobot:
 
         return ur3e
 
-    def inv_kinematics(self, tcp_pos: np.ndarray, tcp_rot: np.ndarray, publish: bool = True) -> np.ndarray:
-        
+    def inv_kinematics(self, tcp_pos: np.ndarray, tcp_rot: np.ndarray, 
+                   publish: bool = True, ignore_all_rotations=False) -> np.ndarray:
+    
         T = SE3(tcp_pos) * SE3.RPY(tcp_rot)
+        num_stuck = int(np.sum(self.stuck_joints))
         
-        # sol.q will only contain angles for the ACTIVE joints
-        sol = self.ur3e.ikine_LM(T)
+        # --- MASK LOGIC ---
+        if ignore_all_rotations:
+            # Strict 3D position only. Great for debugging reachability!
+            mask = [1, 1, 1, 0, 0, 0] 
+        else:
+            mask = [1, 1, 1, 1, 1, 1]
+            for i in range(num_stuck):
+                mask[5 - i] = 0
+                    
+        # Define max retries for IK attempts with random initializations
+        max_retries = 10
+        success = False
         
-        # Reconstruct the full 6-DOF joint array
+        for attempt in range(max_retries):
+            # Generate a random initial guess (q0) within joint limits roughly -pi to pi
+            q0 = np.random.uniform(-np.pi, np.pi, self.ur3e.n)
+            
+            sol = self.ur3e.ikine_LM(T, mask=mask, q0=q0)
+            
+            if sol.success:
+                success = True
+                break # We found a solution, stop searching
+                
+        if not success:
+            print(f"\nWarning: IK totally failed after {max_retries} attempts.")
+            print(f"Reason: {sol.reason}")
+            print("This pose is likely physically unreachable with the current stuck joints.")
+        
+        # Re-introdcue the stuck joints to the commanded joint angles
         full_q = np.zeros(len(self.stuck_joints))
         active_q_idx = 0
         
         for i, is_stuck in enumerate(self.stuck_joints):
             if is_stuck:
-                # Inject the constant stuck angle
                 full_q[i] = self.joint_angles[i]
             else:
-                # Map the next solved active joint angle
                 full_q[i] = sol.q[active_q_idx]
                 active_q_idx += 1
 
@@ -89,9 +114,9 @@ class BreakableRobot:
         return full_q
 
     def update_state(self, body: StuckJointStatus):
-            self.stuck_joints = body.stuck_joints 
-            self.joint_angles = body.joint_positions 
-            self.ur3e = self.init_ur3e()
+        self.stuck_joints = body.stuck_joints 
+        self.joint_angles = body.joint_positions 
+        self.ur3e = self.init_ur3e()
 
 def main():
     # Load config
